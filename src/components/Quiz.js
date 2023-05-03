e94a45c289660fc760483c3e49888c47f88aa3e8
Stephanie Burlinchon <stephanieb170@gmail.com>
2023-05-03 21:08:53 +0000

    1 question per page and click handles added

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,18 +2,38 @@ import React, { useState } from 'react';
 import questions from './Question';
 
 const Quiz = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+
+  const handleAnswerClick = (answerId) => {
+    setSelectedAnswer(answerId);
+  };
+
+  const handleNextQuestion = () => {
+    setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+    setSelectedAnswer(null);
+  };
+
+  const currentQuestion = questions[currentQuestionIndex];
 
   return (
-    <div>
-      {questions.map(id => (
-        <div key={id.index}>
-          <p>Q: {id.text}</p>
-          {id.answers.map((answers) => (
-            <p key={answers.id}>{answers.text}</p>
-          ))}
-          <p>Correct: {id.correctAnswer}</p>
-        </div>
-      ))}
+    <div className='container'>
+      <div className='container-quiz' key={currentQuestion.index}>
+        <p>Question {currentQuestionIndex + 1}: {currentQuestion.text}</p>
+        {currentQuestion.answers.map((answer) => (
+        <button key={answer.id} onClick={() => handleAnswerClick(answer.id)} 
+        className={`answerButton ${selectedAnswer === answer.id ? 'selectedAnswerButton' : ''}`}>
+        {answer.text}
+        </button>
+        ))}
+        <p>Correct: {currentQuestion.correctAnswer}</p>
+      </div>
+      <div>
+        {currentQuestionIndex > 0}
+        {currentQuestionIndex < questions.length - 1 && (
+          <button onClick={handleNextQuestion}>Next</button>
+        )}
+      </div>
     </div>
   );
 };
